4c0c5dea9103ca488656b03a09181786463d3c85
XanJan <annamajberger@gmail.com>
2023-06-10 17:09:32 +0200

    Finished GameRender

--- pacmanclone.py
+++ pacmanclone.py
@@ -4,7 +4,20 @@ import tcod
 
 class GameRender:
     def __init__(self, in_width: int, in_height: int):
-        pygame.init();
-        
+        pygame.init()
+        self._width = in_width
+        self._height = in_height
+        self.screen = pygame.display.set_mode((in_width, in_height))
+        pygame.display.set_caption("Pacman")
+        self._clock = pygame.time.Clock()
+        self._done = False
+        self._game_objects = []
+        self._walls = []
+        self._cookies = []
+        self._hero: Hero = None
+
+    
+
+
             
     
