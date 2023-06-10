44d075de891cda204a1124b267cea52b36d999d3
XanJan <annamajberger@gmail.com>
2023-06-10 17:33:37 +0200

    Added a class for GameObject

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 https://itnext.io/how-to-create-pac-man-in-python-in-300-lines-of-code-or-less-part-1-288d54baf939
 
-Folowing this tutorial for this game. Thought it would be a fun starter project for someone who is new to programming. Like the event TNT
+Following this tutorial for this game. Thought it would be a fun starter project for someone who is new to programming. Like the event TNT
--- pacmanclone.py
+++ pacmanclone.py
@@ -41,5 +41,26 @@ class GameRender:
         pass #!Implement later
 
 
+class GameObject:
+    def __init__(self, in_surface, x, y, in_size: int, in_color=(255, 0, 0), is_circle: bool = False):
+        self._size = in_size
+        self._renderer: GameRender = in_surface
+        self._surface = in_surface._screen
+        self.y = y
+        self.x = x
+        self._color = in_color
+        self._circle = is_circle
+        self._shape = pygame.Rect(self.x, self.y, in_size, in_size)
+
+    def draw(self):
+        if self._circle:
+            pygame.draw.circle(self._surface, self._color, self.x, self.y, self._size)
+        else:
+            rect_object = pygame.Rect(self.x, self.y, self._size, self._size)
+            pygame.draw.rect(self._surface, self._color, rect_object, border_radius=4)
+
+    def tick(self):
+        pass
+
             
     
